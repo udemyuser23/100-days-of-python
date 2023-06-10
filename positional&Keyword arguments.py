29ba6544fc9de941fb7c86ff13d7e2842e48c805
udemyuser23 <129065760+udemyuser23@users.noreply.github.com>
2023-06-11 02:12:05 +0900

    Add files via upload

---
+++ positional&Keyword arguments.py
@@ -0,0 +1,10 @@
+#def greet():
+    #print("Hello Angela")
+    #print("How do you do Jack Bauer?")
+    #greet()
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with(name="Jack", location="London")
