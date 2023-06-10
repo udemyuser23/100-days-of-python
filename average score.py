b7ea2a46565a44dc59dfe2d1b07c18dba119b8ed
udemyuser23 <129065760+udemyuser23@users.noreply.github.com>
2023-06-11 02:16:21 +0900

    Add files via upload

---
+++ average score.py
@@ -0,0 +1,20 @@
+student_heights = ["180", "124", "165",
+                   "173", "189", "169", "146"]
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+print(student_heights)
+
+total_height = 0
+for height in student_heights:
+    total_height += height
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students += 1
+
+average_height = round(total_height / number_of_students)
+print(average_height)
+
+
+
