cdd81863ebd8d96f80ea7e6170ab721f9fef11a2
udemyuser23 <129065760+udemyuser23@users.noreply.github.com>
2023-06-13 08:33:39 +0900

    Add files via upload

---
+++ prime_checker.py
@@ -0,0 +1,9 @@
+def prime_checker(number):
+  is_prime = True
+  for i in range(2, number):
+    if number % i == 0:
+      is_prime = False
+  if is_prime:
+    print("It is a prime number.")
+  else:
+    print("It is not a prime number.")
