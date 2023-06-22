fcb60b7c1bdf014f0efbd6975182bd70b7009384
udemyuser23 <129065760+udemyuser23@users.noreply.github.com>
2023-06-22 19:34:59 +0900

    Add files via upload

---
+++ functions with outputs.py
@@ -0,0 +1,9 @@
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+
+    return(f"{formatted_f_name} {formatted_l_name}")
+
+#format_name("Javokhir", "Ozod")
+#formatted_string = format_name("Javokhir", "Ozod")
+print(format_name("Javokhir", "Ozod"))
