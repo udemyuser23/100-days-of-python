f443f3828496e0236a57b3252ac4f6143cfce1fd
udemyuser23 <129065760+udemyuser23@users.noreply.github.com>
2023-06-26 09:53:32 +0900

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+#Calculator
+
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+    return n1 - n2
+#Multiply
+def multiply(n1, n2):
+    return n1 * n2
+#Divide
+def divide(n1, n2):
+    return n1 / n2
+#Operations
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol}{num2} = {answer}")
